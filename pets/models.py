cae250f9afb2abdf2434c0bd2ba76604bbd2ec37
Garcia <garciasholding@gmail.com>
2023-02-08 17:45:06 -0300

    add: app models and ERD

---
+++ pets/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Sexes(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    DEFAULT = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=20, choices=Sexes.choices, default=Sexes.DEFAULT)
+
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.RESTRICT, related_name="pets", null=False
+    )
+    traits = models.ManyToManyField("traits.Trait", related_name="pets")
